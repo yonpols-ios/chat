868f411ac37b171924af3c3ab283b246af941cee
Juan Pablo Marzetti <jmarzetti@groupon.com>
2015-11-05 14:45:35 -0800

    Initial Commit

---
+++ week3-lab-chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  week3-lab-chat
+//
+//  Created by Juan Pablo Marzetti on 11/5/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ week3-lab-chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week3-lab-chat
+//
+//  Created by Juan Pablo Marzetti on 11/5/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
